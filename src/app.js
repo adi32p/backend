b297926b19ec7e340cbb26acf323a716b104eec6
adi32p <aditya.22210268@viit.ac.in>
2025-04-01 22:21:50 +0530

    all setup configure

--- src/app.js
+++ src/app.js
@@ -6,12 +6,20 @@ const app = express()
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    credentials:true 
+    credentials: true
 }))
 
-app.use(express.json({limit:"16kb"}))
-app.use(express.urlencoded({extended:true,limit:"16kb"}))
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
 
-export {app}
+
+//routes import
+import userRouter from "./routes/user.routes.js"
+
+//routes declaration
+//http://localhost:8000/api/v1/users/register   calling the user.routes.js file
+app.use("/api/v1/users", userRouter)
+
+export default app 
